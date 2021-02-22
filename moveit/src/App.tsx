f81b648acc7bdd47560b69dd4da16755541f02ea
Bleiner Mathias <bleinerbbm@hotmail.com>
2021-02-22 12:58:27 -0300

    :tada: Cria experience bar

---
+++ moveit/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { ExperieceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+
+    <div className="container">
+      <div>
+        <ExperieceBar></ExperieceBar>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ moveit/src/components/ExperienceBar.tsx
@@ -0,0 +1,14 @@
+export function ExperieceBar() {
+    return (
+
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '50%' }} />
+                <span className="curent-experience" style={{ left: '50%' }}>300xp</span>
+            </div>
+            <span>600 xp</span>
+        </header>
+
+    )
+}
